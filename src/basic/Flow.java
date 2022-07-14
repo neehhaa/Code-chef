ab2d91ac67719e7b7f3b06c9cb61958e804406eb
neehhaa <nehawasnik95@outlook.com>
2022-07-14 17:07:48 +0530

    Addition

--- src/basic/Flow.java
+++ src/basic/Flow.java
@@ -5,12 +5,15 @@ public class Flow {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Number of Test Cases : ");
         int testCase = scan.nextInt();
 
         while (testCase-- > 0){
+            System.out.println("Enter Two number : ");
             int Num1 = scan.nextInt();
             int Num2 = scan.nextInt();
             int Addition = Num1 + Num2;
+            System.out.println("Sum is : ");
             System.out.println(Addition);
         }
     }
