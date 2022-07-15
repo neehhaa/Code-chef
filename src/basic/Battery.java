38925ac133897506cd060e6b47030286913a961f
neehhaa <nehawasnik95@outlook.com>
2022-07-15 14:20:26 +0530

    Battery Low

---
+++ src/basic/Battery.java
@@ -0,0 +1,17 @@
+/*
+Battery Low :
+Chef's phone shows a Battery Low notification if the battery level is 15% or less.
+Given that the battery level of Chef's phone is X %,
+determine whether it would show a Battery low notification.
+ */
+
+package basic;
+
+import java.util.Scanner;
+
+public class Battery {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
