686df3b4510c0e661d3b0faca869ad6a8ce3a79a
neehhaa <nehawasnik95@outlook.com>
2022-07-16 20:16:31 +0530

    Enough Space

---
+++ src/math/Space.java
@@ -0,0 +1,26 @@
+//Enough Space
+package math;
+
+import java.util.Scanner;
+
+public class Space {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int testcase = input.nextInt();
+
+        while(testcase-- > 0){
+            int space = input.nextInt();
+            int smallfile = input.nextInt();
+            int bigfile = input.nextInt();
+
+            int totalsize = smallfile + (2 * bigfile);
+            if (space >= totalsize){
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
