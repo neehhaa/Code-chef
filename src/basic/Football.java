4315a90b832545cf3e3bac808bdc54f631e5c19d
neehhaa <nehawasnik95@outlook.com>
2022-07-17 15:33:16 +0530

    Football Cup

---
+++ src/basic/Football.java
@@ -0,0 +1,26 @@
+//Football Cup
+package basic;
+
+import java.util.Scanner;
+
+public class Football {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int testcase = input.nextInt();
+        while(testcase-- > 0){
+            int team1 = input.nextInt();
+            int team2 = input.nextInt();
+            if (team1 >= 1 && team2 >= 1){
+                if (team1 == team2){
+                    System.out.println("YES");
+                }
+                else {
+                    System.out.println("NO");
+                }
+            }
+            else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
