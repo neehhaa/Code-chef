738094f64ec948f840c888c995d45ef5086a8fbd
neehhaa <nehawasnik95@outlook.com>
2022-07-18 17:04:18 +0530

    class

---
+++ src/contest/SecondLargest.java
@@ -0,0 +1,13 @@
+package contest;
+
+import java.util.Scanner;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
+    }
+}
---
+++ src/lunchtime/Pronunciation.java
@@ -0,0 +1,13 @@
+package lunchtime;
+
+import java.util.Scanner;
+
+public class Pronunciation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int testcase = input.nextInt();
+        while (testcase-- > 0) {
+
+        }
+    }
+}
