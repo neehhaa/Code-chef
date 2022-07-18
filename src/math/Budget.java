b69b4b9a78338e7d2a58dd2d82dd1883141e5ea3
neehhaa <nehawasnik95@outlook.com>
2022-07-18 17:24:20 +0530

    Monthly Budget

---
+++ src/math/Budget.java
@@ -0,0 +1,22 @@
+//Monthly Budget
+package math;
+
+import java.util.Scanner;
+
+public class Budget {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int testcase = input.nextInt();
+        while(testcase-- > 0) {
+            int money = input.nextInt();
+            int dailyExp = input.nextInt();
+            if((dailyExp*30) <= money){
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
+
