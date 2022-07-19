7bd975114f57ad091759558ba9d1320f4a5296f5
neehhaa <nehawasnik95@outlook.com>
2022-07-19 16:36:29 +0530

    Mahasena

---
+++ src/basic/Mahasena.java
@@ -0,0 +1,29 @@
+//Mahasena
+package basic;
+
+import java.util.Scanner;
+
+public class Mahasena {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int soldierCount = input.nextInt();
+        int[] soldier = new int[soldierCount];
+        int count = 0;
+
+        for (int i = 0; i <= soldierCount - 1; i++) {
+            soldier[i] = input.nextInt();
+        }
+        for (int i = 0; i <= soldierCount - 1; i++) {
+            if (soldier[i] % 2 == 0) {
+                count++;
+            }
+        }
+        if (count > (soldierCount / 2)) {
+            System.out.println("READY FOR BATTLE");
+        }
+        else {
+            System.out.println("NOT READY");
+        }
+    }
+}
+
