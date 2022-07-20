6e041dd4a83c1cd9902773d07b4ae79875297ddf
neehhaa <nehawasnik95@outlook.com>
2022-07-21 00:57:36 +0530

    Dominant Army

---
+++ src/math/DominantArmy.java
@@ -0,0 +1,23 @@
+//Dominant Army
+package math;
+
+import java.util.Scanner;
+
+public class DominantArmy {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int testcase = input.nextInt();
+        while(testcase-- > 0) {
+            int armyA = input.nextInt();
+            int armyB = input.nextInt();
+            int armyC = input.nextInt();
+            if (armyA > (armyB+armyC) || armyB > (armyA+armyC) ||armyC > (armyB+armyA)){
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
+
+        }
+    }
+}
