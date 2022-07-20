fb1705b307f1a63c8ab83487461337b599121686
neehhaa <nehawasnik95@outlook.com>
2022-07-21 01:03:17 +0530

    Qualify the round

---
+++ src/basic/QualifyTheRound.java
@@ -0,0 +1,26 @@
+package basic;
+
+import java.util.Scanner;
+
+public class QualifyTheRound {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int testcase = input.nextInt();
+
+        while (testcase-- > 0) {
+            int marksneeded = input.nextInt();
+            int a = input.nextInt();
+            int b = input.nextInt();
+
+            int marksgot = a + (b * 2);
+
+            if (marksgot >= marksneeded){
+                System.out.println("Qualify");
+            }
+            else {
+                System.out.println("Not Qualify");
+            }
+        }
+    }
+}
